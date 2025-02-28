64b120db55d5955f16dac100281912474925efcc
Jared Woolston <Jared.Woolston@gmail.com>
2025-02-28 16:35:44 -0700

    Debugging display driver.

--- app/src/main.c
+++ app/src/main.c
@@ -4,7 +4,6 @@
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/adc.h>
@@ -13,8 +12,9 @@
 #include <zephyr/drivers/pwm.h>
 #include <zephyr/sys/util.h>
 #include <zephyr/drivers/display.h>
-#include <lvgl.h>
-#include <lvgl_input_device.h>
+/* <lvgl.h>
+#include <lvgl_mem.h>
+#include <lv_demos.h>*/
 
 #include <zephyr/logging/log.h>
 
@@ -43,30 +43,330 @@ static const struct adc_dt_spec adc_channels[] = {
                          DT_SPEC_AND_COMMA)
 };
 
-const struct device *qdec0 = DEVICE_DT_GET(DT_NODELABEL(pio1_qdec));
+//const struct device *qdec0 = DEVICE_DT_GET(DT_NODELABEL(pio1_qdec));
 
-static uint32_t count;
+//static uint32_t count;
 
-#ifdef CONFIG_LV_Z_ENCODER_INPUT
-static const struct device *lvgl_encoder =
-        DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_encoder_input));
-#endif /* CONFIG_LV_Z_ENCODER_INPUT */
-
-#ifdef CONFIG_LV_Z_KEYPAD_INPUT
-static const struct device *lvgl_keypad =
-        DEVICE_DT_GET(DT_COMPAT_GET_ANY_STATUS_OKAY(zephyr_lvgl_keypad_input));
-#endif /* CONFIG_LV_Z_KEYPAD_INPUT */
-
-static void lv_btn_click_callback(lv_event_t *e)
-{
+/*static void lv_btn_click_callback(lv_event_t* e) {
     ARG_UNUSED(e);
 
     count = 0;
+}*/
+
+
+enum corner {
+	TOP_LEFT,
+	TOP_RIGHT,
+	BOTTOM_RIGHT,
+	BOTTOM_LEFT
+};
+
+typedef void (*fill_buffer)(enum corner corner, uint8_t grey, uint8_t *buf,
+			    size_t buf_size);
+
+
+static void fill_buffer_argb8888(enum corner corner, uint8_t grey, uint8_t *buf,
+				 size_t buf_size)
+{
+	uint32_t color = 0;
+
+	switch (corner) {
+	case TOP_LEFT:
+		color = 0xFFFF0000u;
+		break;
+	case TOP_RIGHT:
+		color = 0xFF00FF00u;
+		break;
+	case BOTTOM_RIGHT:
+		color = 0xFF0000FFu;
+		break;
+	case BOTTOM_LEFT:
+		color = 0xFF000000u | grey << 16 | grey << 8 | grey;
+		break;
+	}
+
+	for (size_t idx = 0; idx < buf_size; idx += 4) {
+		*((uint32_t *)(buf + idx)) = color;
+	}
 }
+
+static void fill_buffer_rgb888(enum corner corner, uint8_t grey, uint8_t *buf,
+			       size_t buf_size)
+{
+	uint32_t color = 0;
+
+	switch (corner) {
+	case TOP_LEFT:
+		color = 0x00FF0000u;
+		break;
+	case TOP_RIGHT:
+		color = 0x0000FF00u;
+		break;
+	case BOTTOM_RIGHT:
+		color = 0x000000FFu;
+		break;
+	case BOTTOM_LEFT:
+		color = grey << 16 | grey << 8 | grey;
+		break;
+	}
+
+	for (size_t idx = 0; idx < buf_size; idx += 3) {
+		*(buf + idx + 0) = color >> 16;
+		*(buf + idx + 1) = color >> 8;
+		*(buf + idx + 2) = color >> 0;
+	}
+}
+
+static uint16_t get_rgb565_color(enum corner corner, uint8_t grey)
+{
+	uint16_t color = 0;
+	uint16_t grey_5bit;
+
+	switch (corner) {
+	case TOP_LEFT:
+		color = 0xF800u;
+		break;
+	case TOP_RIGHT:
+		color = 0x07E0u;
+		break;
+	case BOTTOM_RIGHT:
+		color = 0x001Fu;
+		break;
+	case BOTTOM_LEFT:
+		grey_5bit = grey & 0x1Fu;
+		/* shift the green an extra bit, it has 6 bits */
+		color = grey_5bit << 11 | grey_5bit << (5 + 1) | grey_5bit;
+		break;
+	}
+	return color;
+}
+
+static void fill_buffer_rgb565(enum corner corner, uint8_t grey, uint8_t *buf,
+			       size_t buf_size)
+{
+	uint16_t color = get_rgb565_color(corner, grey);
+
+	for (size_t idx = 0; idx < buf_size; idx += 2) {
+		*(buf + idx + 0) = (color >> 8) & 0xFFu;
+		*(buf + idx + 1) = (color >> 0) & 0xFFu;
+	}
+}
+
+static void fill_buffer_bgr565(enum corner corner, uint8_t grey, uint8_t *buf,
+			       size_t buf_size)
+{
+	uint16_t color = get_rgb565_color(corner, grey);
+
+	for (size_t idx = 0; idx < buf_size; idx += 2) {
+		*(uint16_t *)(buf + idx) = color;
+	}
+}
+
+static void fill_buffer_mono(enum corner corner, uint8_t grey,
+			     uint8_t black, uint8_t white,
+			     uint8_t *buf, size_t buf_size)
+{
+	uint16_t color;
+
+	switch (corner) {
+	case BOTTOM_LEFT:
+		color = (grey & 0x01u) ? white : black;
+		break;
+	default:
+		color = black;
+		break;
+	}
+
+	memset(buf, color, buf_size);
+}
+
+static inline void fill_buffer_mono01(enum corner corner, uint8_t grey,
+				      uint8_t *buf, size_t buf_size)
+{
+	fill_buffer_mono(corner, grey, 0x00u, 0xFFu, buf, buf_size);
+}
+
+static inline void fill_buffer_mono10(enum corner corner, uint8_t grey,
+				      uint8_t *buf, size_t buf_size)
+{
+	fill_buffer_mono(corner, grey, 0xFFu, 0x00u, buf, buf_size);
+}
+
+int sample(void)
+{
+	size_t x;
+	size_t y;
+	size_t rect_w;
+	size_t rect_h;
+	size_t h_step;
+	size_t scale;
+	size_t grey_count;
+	uint8_t bg_color;
+	uint8_t *buf;
+	int32_t grey_scale_sleep;
+	const struct device *display_dev;
+	struct display_capabilities capabilities;
+	struct display_buffer_descriptor buf_desc;
+	size_t buf_size = 0;
+	fill_buffer fill_buffer_fnc = NULL;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device %s not found. Aborting sample.",
+			display_dev->name);
+		return 0;
+	}
+
+	LOG_INF("Display sample for %s", display_dev->name);
+	display_get_capabilities(display_dev, &capabilities);
+
+	if (capabilities.screen_info & SCREEN_INFO_MONO_VTILED) {
+		rect_w = 16;
+		rect_h = 8;
+	} else {
+		rect_w = 2;
+		rect_h = 1;
+	}
+
+	if ((capabilities.x_resolution < 3 * rect_w) ||
+	    (capabilities.y_resolution < 3 * rect_h) ||
+	    (capabilities.x_resolution < 8 * rect_h)) {
+		rect_w = capabilities.x_resolution * 40 / 100;
+		rect_h = capabilities.y_resolution * 40 / 100;
+		h_step = capabilities.y_resolution * 20 / 100;
+		scale = 1;
+	} else {
+		h_step = rect_h;
+		scale = (capabilities.x_resolution / 4) / rect_h;
+	}
+
+	rect_w *= scale;
+	rect_h *= scale;
+
+	if (capabilities.screen_info & SCREEN_INFO_EPD) {
+		grey_scale_sleep = 10000;
+	} else {
+		grey_scale_sleep = 100;
+	}
+
+	if (capabilities.screen_info & SCREEN_INFO_X_ALIGNMENT_WIDTH) {
+		rect_w = capabilities.x_resolution;
+	}
+
+	buf_size = rect_w * rect_h;
+
+	if (buf_size < (capabilities.x_resolution * h_step)) {
+		buf_size = capabilities.x_resolution * h_step;
+	}
+
+	switch (capabilities.current_pixel_format) {
+	case PIXEL_FORMAT_ARGB_8888:
+		bg_color = 0x00u;
+		fill_buffer_fnc = fill_buffer_argb8888;
+		buf_size *= 4;
+		break;
+	case PIXEL_FORMAT_RGB_888:
+		bg_color = 0xFFu;
+		fill_buffer_fnc = fill_buffer_rgb888;
+		buf_size *= 3;
+		break;
+	case PIXEL_FORMAT_RGB_565:
+		bg_color = 0xFFu;
+		fill_buffer_fnc = fill_buffer_rgb565;
+		buf_size *= 2;
+		break;
+	case PIXEL_FORMAT_BGR_565:
+		bg_color = 0xFFu;
+		fill_buffer_fnc = fill_buffer_bgr565;
+		buf_size *= 2;
+		break;
+	case PIXEL_FORMAT_MONO01:
+		bg_color = 0xFFu;
+		fill_buffer_fnc = fill_buffer_mono01;
+		buf_size = DIV_ROUND_UP(DIV_ROUND_UP(
+			buf_size, NUM_BITS(uint8_t)), sizeof(uint8_t));
+		break;
+	case PIXEL_FORMAT_MONO10:
+		bg_color = 0x00u;
+		fill_buffer_fnc = fill_buffer_mono10;
+		buf_size = DIV_ROUND_UP(DIV_ROUND_UP(
+			buf_size, NUM_BITS(uint8_t)), sizeof(uint8_t));
+		break;
+	default:
+		LOG_ERR("Unsupported pixel format. Aborting sample.");
+		return 0;
+	}
+
+	buf = k_malloc(buf_size);
+
+	if (buf == NULL) {
+		LOG_ERR("Could not allocate memory. Aborting sample.");
+		return 0;
+	}
+
+	(void)memset(buf, bg_color, buf_size);
+
+	buf_desc.buf_size = buf_size;
+	buf_desc.pitch = capabilities.x_resolution;
+	buf_desc.width = capabilities.x_resolution;
+	buf_desc.height = h_step;
+
+	/*
+	 * The following writes will only render parts of the image,
+	 * so turn this option on.
+	 * This allows double-buffered displays to hold the pixels
+	 * back until the image is complete.
+	 */
+	buf_desc.frame_incomplete = true;
+
+	for (int idx = 0; idx < capabilities.y_resolution; idx += h_step) {
+		/*
+		 * Tweaking the height value not to draw outside of the display.
+		 * It is required when using a monochrome display whose vertical
+		 * resolution can not be divided by 8.
+		 */
+		if ((capabilities.y_resolution - idx) < h_step) {
+			buf_desc.height = (capabilities.y_resolution - idx);
+		}
+		display_write(display_dev, 0, idx, &buf_desc, buf);
+	}
+
+	buf_desc.pitch = rect_w;
+	buf_desc.width = rect_w;
+	buf_desc.height = rect_h;
+
+	fill_buffer_fnc(TOP_LEFT, 0, buf, buf_size);
+	x = 0;
+	y = 0;
+	display_write(display_dev, x, y, &buf_desc, buf);
+
+	fill_buffer_fnc(TOP_RIGHT, 0, buf, buf_size);
+	x = capabilities.x_resolution - rect_w;
+	y = 0;
+	display_write(display_dev, x, y, &buf_desc, buf);
+
+	/*
+	 * This is the last write of the frame, so turn this off.
+	 * Double-buffered displays will now present the new image
+	 * to the user.
+	 */
+	buf_desc.frame_incomplete = false;
+
+	fill_buffer_fnc(BOTTOM_RIGHT, 0, buf, buf_size);
+	x = capabilities.x_resolution - rect_w;
+	y = capabilities.y_resolution - rect_h;
+	display_write(display_dev, x, y, &buf_desc, buf);
+
+	display_blanking_off(display_dev);
+
+	LOG_INF("Display starts");
+	return 0;
+}
+
 
 int main(void) {
     int err;
-    uint32_t count = 0;
+    //uint32_t count = 0;
     uint16_t buf;
     struct adc_sequence sequence = {
         .buffer = &buf,
@@ -98,88 +398,76 @@ int main(void) {
     }
     //ret = gpio_pin_set_dt(&led, true);
 
-    char count_str[11] = {0};
-    const struct device *display_dev;
-    lv_obj_t *hello_world_label;
-    lv_obj_t *count_label;
+    //char count_str[11] = {0};
 
-    display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+    /*const struct device* display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
     if (!device_is_ready(display_dev)) {
         LOG_ERR("Device not ready, aborting test");
         return 0;
+    }*/
+
+    /*uint16_t Color = 0xFFFF;
+    uint16_t j;
+    uint16_t Image[240*240];
+
+    Color = ((Color<<8)&0xff00)|(Color>>8);
+
+    for (j = 0; j < 240*240; j++) {
+        Image[j] = Color;
     }
 
-#ifdef CONFIG_LV_Z_ENCODER_INPUT
-    lv_obj_t *arc;
-    lv_group_t *arc_group;
+    struct display_buffer_descriptor desc;
+    desc.buf_size = 240 * 240 * 2;
+    desc.width = 240;
+    desc.height = 240;
+    desc.pitch = 240;
+    desc.frame_incomplete = false;
 
-    arc = lv_arc_create(lv_screen_active());
-    lv_obj_align(arc, LV_ALIGN_CENTER, 0, -15);
-    lv_obj_set_size(arc, 150, 150);
+    display_write(display_dev, 0, 0, &desc, (void *)Image);*/
 
-    arc_group = lv_group_create();
-    lv_group_add_obj(arc_group, arc);
-    lv_indev_set_group(lvgl_input_get_indev(lvgl_encoder), arc_group);
-#endif /* CONFIG_LV_Z_ENCODER_INPUT */
-
-#ifdef CONFIG_LV_Z_KEYPAD_INPUT
-    lv_obj_t *btn_matrix;
-    lv_group_t *btn_matrix_group;
-    static const char *const btnm_map[] = {"1", "2", "3", "4", ""};
-
-    btn_matrix = lv_buttonmatrix_create(lv_screen_active());
-    lv_obj_align(btn_matrix, LV_ALIGN_CENTER, 0, 70);
-    lv_buttonmatrix_set_map(btn_matrix, (const char **)btnm_map);
-    lv_obj_set_size(btn_matrix, 100, 50);
-
-    btn_matrix_group = lv_group_create();
-    lv_group_add_obj(btn_matrix_group, btn_matrix);
-    lv_indev_set_group(lvgl_input_get_indev(lvgl_keypad), btn_matrix_group);
-#endif /* CONFIG_LV_Z_KEYPAD_INPUT */
-
-    if (IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
-        lv_obj_t *hello_world_button;
-
-        hello_world_button = lv_button_create(lv_screen_active());
-        lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, -15);
-        lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED,
-                            NULL);
-        hello_world_label = lv_label_create(hello_world_button);
-    } else {
-        hello_world_label = lv_label_create(lv_screen_active());
-    }
+    /*lv_obj_t* hello_world_button = lv_button_create(lv_screen_active());
+    lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, -15);
+    lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED, NULL);
+    lv_obj_t* hello_world_label = lv_label_create(hello_world_button);
 
     lv_label_set_text(hello_world_label, "Hello world!");
     lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 
-    count_label = lv_label_create(lv_screen_active());
+    lv_obj_t* count_label = lv_label_create(lv_screen_active());
     lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+    lv_obj_set_style_bg_color(hello_world_label, lv_color_make(255, 0, 0), LV_STATE_DEFAULT);
 
     lv_timer_handler();
+    display_blanking_on(display_dev);
+    k_sleep(K_MSEC(120));
     display_blanking_off(display_dev);
-
+    k_sleep(K_MSEC(1000));
+    lvgl_print_heap_info(false);*/
+    sample();
     while (1) {
-        if ((count % 100) == 0U) {
-            sprintf(count_str, "%d", count/100U);
+        /*f (lv_screen_active() == NULL) {
+            LOG_ERR("No display attached");
+        }*/
+        /*if ((count % 10) == 0U) {
+            sprintf(count_str, "%d", count / 100U);
             lv_label_set_text(count_label, count_str);
         }
-        lv_timer_handler();
-        ++count;
+        lv_timer_handler();*/
+        //uint32_t sleep_ms = lv_timer_handler();
+
+        //k_msleep(MIN(sleep_ms, INT32_MAX));
+        //++count;
         k_sleep(K_MSEC(10));
-        k_sleep(K_MSEC(1000));
-        printk("ADC reading[%u]:\n", count++);
+        k_sleep(K_MSEC(490));
+        //LOG_INF("ADC reading[%u]:\n", count++);
         for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
             int32_t val_mv;
-
-            printk("- %s, channel %d: ",
-                   adc_channels[i].dev->name,
-                   adc_channels[i].channel_id);
 
             (void)adc_sequence_init_dt(&adc_channels[i], &sequence);
 
             err = adc_read_dt(&adc_channels[i], &sequence);
             if (err < 0) {
-                printk("Could not read (%d)\n", err);
+                LOG_ERR("Could not read (%d)\n", err);
                 continue;
             }
 
@@ -193,15 +481,15 @@ int main(void) {
             } else {
                 val_mv = (int32_t)buf;
             }
-            printk("%"PRId32, val_mv);
             err = adc_raw_to_millivolts_dt(&adc_channels[i],
                                            &val_mv);
             val_mv = (val_mv * 3) / 2;
             /* conversion to mV may not be supported, skip if not */
             if (err < 0) {
-                printk(" (value in mV not available)\n");
-            } else {
-                printk(" = %"PRId32" mV\n", val_mv);
+                LOG_ERR(" (value in mV not available)\n");
+            /*} else {
+                LOG_INF("- %s, channel %d: %"PRId32" mV\n", adc_channels[i].dev->name,
+                        adc_channels[i].channel_id, val_mv);*/
             }
         }
     }
