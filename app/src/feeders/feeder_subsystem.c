2fe885c89ec868a6392de8c2a862b27ed202bba6
Jared Woolston <Jared.Woolston@gmail.com>
2025-03-11 20:27:00 -0600

    Achieved a working feeder subsystem combining a queue sensor monitor thread with controls on the feeder motors.

--- app/src/feeders/feeder_subsystem.c
+++ app/src/feeders/feeder_subsystem.c
@@ -1,7 +1,3 @@
-//
-// Created by Jared Woolston on 3/10/2025.
-//
-
 #include "feeder_subsystem.h"
 
 #include <app/drivers/motor.h>
@@ -40,7 +36,15 @@ static k_tid_t adc_thread_tid;
 
 static const struct device* motor_dev;
 
-static int initialize_feeder_sample_adc(void) {
+static bool is_valid_feeder(const int feeder) {
+    if (feeder != BRASS && feeder != BULLET) {
+        LOG_ERR("Invalid feeder index: %d", feeder);
+        return false;
+    }
+    return true;
+}
+
+static int initialize_feeder_adc(void) {
     LOG_DBG("Initializing ADC.");
     if (!adc_is_ready_dt(&adc_channels[0])) {
         LOG_ERR("ADC device is not ready");
@@ -62,8 +66,8 @@ static int initialize_feeder_sample_adc(void) {
     return 0;
 }
 
-static int feeder_sensor_sample_init(void*, void*, void*) {
-    LOG_DBG("Initializing feeder sensor sample task");
+static int feeder_sensor_init(void*, void*, void*) {
+    LOG_DBG("Initializing feeder sensor task");
     struct adc_sequence_options options = {
         .extra_samplings = 0,
         .interval_us = 0
@@ -76,51 +80,91 @@ static int feeder_sensor_sample_init(void*, void*, void*) {
         .resolution = ADC_RESOLUTION_12B,
     };
 
-    initialize_feeder_sample_adc();
-    adc_sequence_init_dt(&adc_channels[0], &sequence);
+    initialize_feeder_adc();
+    adc_sequence_init_dt(&adc_channels[BRASS], &sequence);
 
     if (adc_channels_count > 1) {
-        sequence.channels |= BIT(adc_channels[1].channel_id);
+        sequence.channels |= BIT(adc_channels[BULLET].channel_id);
     }
 
     while (1) {
-        LOG_DBG("Triggering ADC Read.");
         int ret = adc_read_dt(&adc_channels[0], &sequence);
         if (ret) {
-            LOG_ERR("adc_read() failed with code %d", ret);
+            LOG_ERR("adc_read_dt() failed with code %d", ret);
             return ret;
         }
 
         for (int i = 0; i < adc_channels_count; i++) {
             int32_t sample_value = m_sample_buffer[i];
-            ret = adc_raw_to_millivolts(adc_ref_internal(adc_channels[i].dev), adc_channels[i].channel_cfg.gain, sequence.resolution,
-                                        &sample_value);
+            ret = adc_raw_to_millivolts(adc_ref_internal(adc_channels[i].dev), adc_channels[i].channel_cfg.gain,
+                sequence.resolution, &sample_value);
 
-            /* conversion to mV may not be supported, skip if not */
-            if ((ret < 0) || adc_channels[i].vref_mv) {
-                printf(" (value in mV not available)\n");
+            // Conversion to mV may not be supported, skip if not
+            if (ret < 0 || adc_channels[i].vref_mv) {
+                LOG_ERR("Value in mV not available. Error: %d", ret);
             } else {
                 LOG_DBG("Channel %d reading: %d mV", i, sample_value);
+                if (sample_value > CONFIG_FEEDER_FULL_SENSOR_THRESHOLD_MV) {
+                    LOG_DBG("Channel %d is full", i);
+                    motor_off(motor_dev, i);
+                } else {
+                    motor_on(motor_dev, i);
+                }
             }
         }
-
+        // Sleep until we care about checking again
         k_sleep(K_MSEC(500));
     }
     return 0;
 }
 
 int feeder_subsystem_init() {
-    LOG_DBG("Searching for motor device.");
     motor_dev = DEVICE_DT_GET(DT_NODELABEL(motors));
     if (!device_is_ready(motor_dev)) {
         LOG_ERR("Motor device not ready");
         return -ENODEV;
     }
+    LOG_DBG("Activating feeders");
+    set_feeder_speed(BRASS, 25);
+    set_feeder_speed(BULLET, 75);
+    start_feeder(BRASS);
+    start_feeder(BULLET);
 
+    LOG_DBG("Starting feeder queue monitor thread.");
     adc_thread_tid = k_thread_create(&adc_thread_data, adc_thread_stack,
                                      K_THREAD_STACK_SIZEOF(adc_thread_stack),
-                                     feeder_sensor_sample_init,
+                                     feeder_sensor_init,
                                      NULL, NULL, NULL,
-                                     -1, K_ESSENTIAL | K_FP_REGS, K_NO_WAIT);
+                                     CONFIG_FEEDER_FULL_SENSOR_THREAD_PRIORITY,
+                                     K_ESSENTIAL | K_FP_REGS, K_NO_WAIT);
     return 0;
+}
+
+int start_feeder(const int feeder) {
+    if (!is_valid_feeder(feeder)) {
+        return -EINVAL;
+    }
+    LOG_DBG("Starting feeder %d", feeder);
+    return motor_on(motor_dev, feeder);
+}
+
+int stop_feeder(const int feeder) {
+    if (!is_valid_feeder(feeder)) {
+        return -EINVAL;
+    }
+    LOG_DBG("Stopping feeder %d", feeder);
+    return motor_off(motor_dev, feeder);
+}
+
+int set_feeder_speed(const int feeder, const int speed) {
+    if (!is_valid_feeder(feeder)) {
+
+        return -EINVAL;
+    }
+    if (speed < 0 || speed > 100) {
+        LOG_ERR("Invalid speed value %d. Must be in the range of [0-100].", speed);
+        return -EINVAL;
+    }
+    LOG_DBG("Setting feeder %d speed to %d", feeder, speed);
+    return motor_set_speed(motor_dev, feeder, speed);
 }
--- app/src/feeders/feeder_subsystem.h
+++ app/src/feeders/feeder_subsystem.h
@@ -1,10 +1,47 @@
-//
-// Created by Jared Woolston on 3/10/2025.
-//
-
 #ifndef FEEDER_SUBSYSTEM_H
 #define FEEDER_SUBSYSTEM_H
 
+#define BRASS 0 /**< The channel corresponding to the brass feeder. ADC and Motor channels line up. */
+#define BULLET 1 /**< The channel corresponding to the bullet feeder. ADC and Motor channels line up. */
+
+/**
+ * @brief Initialize the feeder subsystem.
+ *
+ * Upon initialization, the feeder subsystem will begin monitoring the queue full sensors and conditionally applying
+ * activation settings based on if the app has enabled or disabled a specific channel.
+ *
+ * @return 0 on success or errno code on failure.
+ */
 int feeder_subsystem_init();
+
+/**
+ * @brief Activates (starts) a specific feeder.
+ *
+ * Once activated, a feeder will engage its motor. If queue full sensor triggers, the motor will automatically
+ * disengage. If the queue full sensor clears, the motor will automatically re-engage.
+ *
+ * @param feeder The channel to start. Must be one of BRASS or BULLET
+ * @return 0 on success or errno code on failure.
+ */
+int start_feeder(int feeder);
+
+/**
+ * @brief Deactivates (stops) a specific feeder.
+ *
+ * Once deactivated, a feeder will not engage its motor, no matter the state of its queue full sensor.
+ *
+ * @param feeder The channel to stop. Must be one of BRASS or BULLET
+ * @return 0 on success or errno code on failure.
+ */
+int stop_feeder(int feeder);
+
+/**
+ * @brief Set a specific feeders speed.
+ *
+ * @param feeder The channel to stop. Must be one of BRASS or BULLET
+ * @param speed The speed percentage to set the feeder to. Must be in the range of [0-100]
+ * @return 0 on success or errno code on failure.
+ */
+int set_feeder_speed(int feeder, int speed);
 
 #endif //FEEDER_SUBSYSTEM_H
